b8d14e83a97063206b1e90a212862fc4e2ee3a7a
Ankit99singh <ankit.singh99.in@gmail.com>
2025-03-02 23:43:42 +0530

    router

---
+++ 07Route/src/components/index.js
@@ -0,0 +1,9 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import About from "./About";
+import Home from "./Home";
+import Contact from "./Contact";
+import User from "./User"
+import Github from "./GitHub";
+
+export {Header, Footer, About, Contact, Home,User, Github}
